addf6e481960a19a6020fd78637e4e9dc445e933
Dilshod Fayzullayev <brogrammer.devs@gmail.com>
2025-11-08 20:06:26 +0500

    initialize Spring Boot Todo application with Maven setup

---
+++ src/test/java/uz/algoexpert/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.algoexpert.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
